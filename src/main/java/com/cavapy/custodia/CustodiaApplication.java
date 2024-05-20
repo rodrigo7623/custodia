88e6369ac10f01b3924b6c667798009799060256
rodrigo7623 <rodrigo7623@gmail.com>
2024-05-20 15:05:37 -0400

    primer commit

---
+++ src/main/java/com/cavapy/custodia/CustodiaApplication.java
@@ -0,0 +1,13 @@
+package com.cavapy.custodia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustodiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustodiaApplication.class, args);
+	}
+
+}
